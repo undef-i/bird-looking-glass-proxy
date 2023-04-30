4ef7802fc01724334ef6cfce9d43796928a9eb9f
undef-i <lamts@tuta.io>
2023-04-30 19:24:45 +0800

    Fix existing issues

--- src/route/bird.rs
+++ src/route/bird.rs
@@ -16,7 +16,76 @@ pub async fn bird(_param: web::Query<Param>, _req: HttpRequest, mode: i8) -> Res
     });
     let mut connection = block_on(client.connect())?;
     let messages = block_on(connection.send_request(&_param.q))?;
-    return Ok(messages.iter().map(|message |format!("{:?}",message)).collect::<String>());
+    return Ok(messages.iter().map(|message | match message {
+        Message::Welcome(s) |
+        Message::ReadingConfiguration(s) |
+        Message::Reconfigured(s) |
+        Message::ReconfigurationInProgress(s) |
+        Message::ReconfigurationQueued(s) |
+        Message::ReconfigurationIgnoredShutdown(s) |
+        Message::ShutdownOrdered(s) |
+        Message::AlreadyDisable(s) |
+        Message::Disabled(s) |
+        Message::AlreadyEnabled(s) |
+        Message::Enabled(s) |
+        Message::Restarted(s) |
+        Message::StatusReport(s) |
+        Message::RouteCount(s) |
+        Message::Reloading(s) |
+        Message::AccessRestricted(s) |
+        Message::ReconfigurationUnQueued(s) |
+        Message::ReconfigurationConfirmed(s) |
+        Message::NothingToDo(s) |
+        Message::ConfigurationOk(s) |
+        Message::UndoRequested(s) |
+        Message::UndoScheduled(s) |
+        Message::EvaluatingExpression(s) |
+        Message::GracefulRestartStatus(s) |
+        Message::BirdVersion(s) |
+        Message::InterfaceList(s) |
+        Message::ProtocolList(s) |
+        Message::InterfaceAddress(s) |
+        Message::InterfaceFlags(s) |
+        Message::InterfaceSummary(s) |
+        Message::ProtocolDetails(s) |
+        Message::RouteList(s) |
+        Message::RouteDetails(s) |
+        Message::StaticRouteList(s) |
+        Message::SymbolList(s) |
+        Message::Uptime(s) |
+        Message::RouteExtendedAttributeList(s) |
+        Message::OspfNeighbors(s) |
+        Message::Ospf(s) |
+        Message::OspfInterface(s) |
+        Message::OspfState(s) |
+        Message::OspfLsadb(s) |
+        Message::Memory(s) |
+        Message::RoaList(s) |
+        Message::BfdSessions(s) |
+        Message::RipInterfaces(s) |
+        Message::RipNeighbors(s) |
+        Message::BabelInterfaces(s) |
+        Message::BabelNeighbors(s) |
+        Message::BabelEntries(s) |
+        Message::ProtocolListHeader(s) |
+        Message::InterfaceSummaryHeader(s) |
+        Message::TableHeader(_, s) |
+        Message::ReplyTooLong(s) |
+        Message::RouteNotFound(s) |
+        Message::ConfigurationFileError(s) |
+        Message::NoProtocolsMatch(s) |
+        Message::StoppedDueToReconfiguration(s) |
+        Message::ProtocolDown(s) |
+        Message::ReloadFailed(s) |
+        Message::AccessDenied(s) |
+        Message::RuntimeError(_, s) |
+        Message::CommandTooLong(s) |
+        Message::ParseError(s) |
+        Message::InvalidSymbol(s) |
+        Message::ClientError(_, s) |
+        Message::Unknown(_, s) => s.to_string(),
+        _ => "".to_string(),
+    }).collect::<Vec<String>>().join("\n"));
 
 }
 pub async fn bird4(_param: web::Query<Param>, _req: HttpRequest) -> impl Responder {
